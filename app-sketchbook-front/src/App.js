c6c683b8553a17163f96a2ac3a9d02c1d748f042
Ejo415 <Ejo7220@gmail.com>
2020-12-15 14:57:36 -0500

    added home import

--- app-sketchbook-front/src/App.js
+++ app-sketchbook-front/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 import UsersIndex from "./components/UsersIndex";
 //import PlayersIndex from "./components/PlayersIndex";
 //import PlayersIndex from "./components/PlayersIndex";
