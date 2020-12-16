710df0f3b6a9a3bdb6d86a044376a5e8a75d4883
Ejo415 <Ejo7220@gmail.com>
2020-12-16 14:37:15 -0500

    added new path

--- app-sketchbook-front/src/App.js
+++ app-sketchbook-front/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import ProjectsIndex from "./components/ProjectsIndex";
 import UsersIndex from "./components/UsersIndex";
-//import PlayersIndex from "./components/PlayersIndex";
+import UserForm from "./components/UserForm";
 //import PlayersIndex from "./components/PlayersIndex";
 
 
@@ -18,7 +17,7 @@ function App() {
          <Route exact path="/" component={Home} />
          <Route path= "/users" component={UsersIndex} />
          <Route path= "/projects" component={ProjectsIndex} />
-         <Route />
+         <Route path= "/user/new" component={UserForm} />
        </Switch>
      </Router>
     </div>
