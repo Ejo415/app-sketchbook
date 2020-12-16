5ab7b4ddff73ab74d235edbd8d4961dedb508867
Ejo415 <Ejo7220@gmail.com>
2020-12-16 14:37:38 -0500

    created user form

--- app-sketchbook-front/src/components/UserForm.js
+++ app-sketchbook-front/src/components/UserForm.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const UserForm = () => {
-    return (
+export default class UserForm extends Component {
+    state = {
+        name: ''
+    }
+
+    submit = (e) => {
+        e.preventDefault()
+        this.setState({
+            name: ''
+        })
+    }
+    render() { 
+        return (
         <div>
-            <h4>Add User</h4>
+            <h1>Add User</h1>
+            <form onSubmit={this.submit}>
+               Name: <input onChange={e => this.setState({name: e.target.value})} type="text" value={this.state.name} />
+            <input type="submit" value="Create User" />
+            </form>
         </div>
+        
     );
+   } 
 }
-
-export default UserForm;
