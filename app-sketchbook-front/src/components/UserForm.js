4eac3505a07d2c34844eeff156c3cf388817a643
Ejo415 <Ejo7220@gmail.com>
2020-12-16 17:43:58 -0500

    built out form

--- app-sketchbook-front/src/components/UserForm.js
+++ app-sketchbook-front/src/components/UserForm.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { createUser } from "../redux/actions/userActions";
+import { connect } from "react-redux";
+class UserForm extends Component {
+  state = {
+    name: "",
+  };
 
-export default class UserForm extends Component {
-    state = {
-        name: ''
-    }
+  submit = (e) => {
+    e.preventDefault();
+    this.props.createUser(this.state);
+    this.setState({
+      name: "",
+    });
+    this.props.history.push("/users");
+  };
 
-    submit = (e) => {
-        e.preventDefault()
-        this.setState({
-            name: ''
-        })
-    }
-    render() { 
-        return (
-        <div>
-            <h1>Add User</h1>
-            <form onSubmit={this.submit}>
-               Name: <input onChange={e => this.setState({name: e.target.value})} type="text" value={this.state.name} />
-            <input type="submit" value="Create User" />
-            </form>
-        </div>
-        
+  render() {
+    return (
+      <div>
+        <h1>Add User</h1>
+        <form onSubmit={this.submit}>
+          Name:{" "}
+          <input
+            onChange={(e) => this.setState({ name: e.target.value })}
+            type="text"
+            value={this.state.name}
+          />
+          <input type="submit" value="Create User" />
+        </form>
+      </div>
     );
-   } 
+  }
 }
+
+export default connect(null, { createUser })(UserForm);
