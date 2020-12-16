5c70c8618c2d04414129d17f115ae22ddeff1f89
Ejo415 <Ejo7220@gmail.com>
2020-12-16 17:44:28 -0500

    built first get and post functions

--- app-sketchbook-front/src/redux/actions/userActions.js
+++ app-sketchbook-front/src/redux/actions/userActions.js
@@ -1,9 +1,25 @@
 export const getUsers = () => {
-    return (dispatch) => {
-        fetch('http://localhost:3001/users')
-        .then((res) => res.json())
-        .then((players) =>
-            dispatch({ type: "FETCH_USERS_SUCCESS", payload: players})
-            );
-    };
+  return (dispatch) => {
+    fetch("http://localhost:3001/users")
+      .then((res) => res.json())
+      .then((users) =>
+        dispatch({ type: "FETCH_USERS_SUCCESS", payload: users })
+      );
+  };
 };
+
+export const createUser = (data) => {
+    return (dispatch) => {
+      fetch("http://localhost:3001/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ user: data }),
+      })
+        .then((res) => res.json())
+        .then((user) =>
+          dispatch({ type: "CREATE_USER_SUCCESS", payload: user })
+        );
+    };
+  };
