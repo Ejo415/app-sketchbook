60ea803e9c7258610872bc58c0e3d0a289d89828
Ejo415 <Ejo7220@gmail.com>
2020-12-16 17:10:41 -0500

    updated cors and added before action in controller

--- app-sketchbook-back/app/controllers/users_controller.rb
+++ app-sketchbook-back/app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  
  def index 
   users = User.all
 
   render :json => users, except: [:password_digest]
  end
 
+ def create 
+  byebug
+  user = User.new(user_params)
+  if player.save 
+    render json: player
+  end
+ end
 
+private
+
+def user_params
+  params.require(:user).permit(:name)
+end
 
 end
