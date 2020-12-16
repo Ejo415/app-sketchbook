70a6f5441e589a92d175cca0dcdbb0ec284f3f58
Ejo415 <Ejo7220@gmail.com>
2020-12-16 14:37:28 -0500

    added new nav link

--- app-sketchbook-front/src/components/Nav.js
+++ app-sketchbook-front/src/components/Nav.js
@@ -7,13 +7,16 @@ const Nav = () => {
         <div>
         <ul>
              <li>
-             <Link to='/' >Home</Link>
+                <Link to='/' >Home</Link>
              </li>
              <li>
-             <Link to='/users' >Users</Link>
+                <Link to='/users' >Users</Link>
              </li>
              <li>
-             <Link to='/projects' >Projects</Link>
+                <Link to='/projects' >Projects</Link>
+             </li>
+             <li>
+                 <Link to='/user/new' >Create a user</Link>
              </li>
         </ul>
             
