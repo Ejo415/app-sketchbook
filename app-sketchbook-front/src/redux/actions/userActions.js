bd88d1502a55ff07ee39e41d8c1f1d12dadea0bf
Ejo415 <ejo7220@yahoo.com>
2020-12-17 17:12:08 -0500

    added getuser const. will remove

--- app-sketchbook-front/src/redux/actions/userActions.js
+++ app-sketchbook-front/src/redux/actions/userActions.js
@@ -8,18 +8,26 @@ export const getUsers = () => {
   };
 };
 
-export const createUser = (data) => {
-    return (dispatch) => {
-      fetch("http://localhost:3001/users", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ user: data }),
-      })
-        .then((res) => res.json())
-        .then((user) =>
-          dispatch({ type: "CREATE_USER_SUCCESS", payload: user })
-        );
-    };
+export const getUser = () => {
+  return (dispatch) => {
+    fetch("http://localhost:3001/users/:id")
+      .then((res) => res.json())
+      .then((user) =>
+        dispatch({ type: "FETCH_USER_SUCCESS", payload: user })
+      );
   };
+};
+
+export const createUser = (data) => {
+  return (dispatch) => {
+    fetch("http://localhost:3001/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: data }),
+    })
+      .then((res) => res.json())
+      .then((user) => dispatch({ type: "CREATE_USER_SUCCESS", payload: user }));
+  };
+};
