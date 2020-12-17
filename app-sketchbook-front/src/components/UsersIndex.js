a5cb334a0ff677d86c124324719428952f63302e
Ejo415 <ejo7220@yahoo.com>
2020-12-17 17:11:05 -0500

    formatted

--- app-sketchbook-front/src/components/UsersIndex.js
+++ app-sketchbook-front/src/components/UsersIndex.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import {getUsers} from '../redux/actions/userActions'
-import UserListItem from './UserListItem'
+import React from "react";
+import { connect } from "react-redux";
+import { getUsers } from "../redux/actions/userActions";
+import UserListItems from "./UserListItems";
 class UsersIndex extends React.Component {
-    componentDidMount() {
-        this.props.getUsers()
-    }
-    render() {
-
-        return (
-            <div>
-                <h1>Users:</h1>
-                {this.props.users.map(user => <UserListItem user={user} key={user.id} />)}
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.props.getUsers();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Users:</h1>
+        {this.props.users.map((user) => (
+          <UserListItems user={user} key={user.id} />
+        ))}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = ({users}) => {
-    return {
-        users: users.all
-    }
-}
+const mapStateToProps = ({ users }) => {
+  return {
+    users: users.all,
+  };
+};
 
-
-export default connect(mapStateToProps, {getUsers})(UsersIndex)
+export default connect(mapStateToProps, { getUsers })(UsersIndex);
