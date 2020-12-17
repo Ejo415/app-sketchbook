3a6e5b58ed7a4a2bdac0c69a2a3c24aa9ac8aa87
Ejo415 <ejo7220@yahoo.com>
2020-12-17 13:00:04 -0500

    added and adjusted routes

--- app-sketchbook-front/src/App.js
+++ app-sketchbook-front/src/App.js
@@ -1,25 +1,27 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import ProjectsIndex from "./components/ProjectsIndex";
 import UsersIndex from "./components/UsersIndex";
 import UserForm from "./components/UserForm";
-//import PlayersIndex from "./components/PlayersIndex";
-
+import ProjectForm from "./components/ProjectForm";
+import UserShow from "./components/UserShow";
 
 function App() {
   return (
     <div className="App">
-     <Router>
-     <Nav />
-       <Switch>
-         <Route exact path="/" component={Home} />
-         <Route path= "/users" component={UsersIndex} />
-         <Route path= "/projects" component={ProjectsIndex} />
-         <Route path= "/user/new" component={UserForm} />
-       </Switch>
-     </Router>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/users" component={UsersIndex} />
+          <Route path="/projects" component={ProjectsIndex} />
+          <Route path="/user/new" component={UserForm} />
+          <Route path="/project/new" component={ProjectForm} />
+          <Route path="/users/:id" component={UserShow} />
+        </Switch>
+      </Router>
     </div>
   );
 }
