fca710d17a006f2b9071d901df022ddbfb685795
Ejo415 <ejo7220@yahoo.com>
2020-12-17 17:07:48 -0500

    edited routes

--- app-sketchbook-front/src/App.js
+++ app-sketchbook-front/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
-import ProjectsIndex from "./components/ProjectsIndex";
 import UsersIndex from "./components/UsersIndex";
 import UserForm from "./components/UserForm";
 import ProjectForm from "./components/ProjectForm";
@@ -16,9 +15,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/users" component={UsersIndex} />
-          <Route path="/projects" component={ProjectsIndex} />
           <Route path="/user/new" component={UserForm} />
-          <Route path="/project/new" component={ProjectForm} />
+          <Route path="/project/:id/new" component={ProjectForm} />
           <Route path="/users/:id" component={UserShow} />
         </Switch>
       </Router>
