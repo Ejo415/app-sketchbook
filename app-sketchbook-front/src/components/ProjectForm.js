a888cc9ad5ab508c8cd03271955ecbf5c620ccb6
Ejo415 <ejo7220@yahoo.com>
2020-12-17 17:09:27 -0500

    got form working and linked to user

--- app-sketchbook-front/src/components/ProjectForm.js
+++ app-sketchbook-front/src/components/ProjectForm.js
@@ -9,21 +9,22 @@ class ProjectForm extends Component {
 
   submit = (e) => {
     e.preventDefault();
-    this.props.createProject(this.state);
+    console.log(this.props.user);
+    this.props.createProject(this.state, this.props.user);
     this.setState({
       concept: "",
     });
-    //this.props.history.push("/users");
+    this.props.history.push(`/users/${this.props.user.id}`);
   };
 
   render() {
     return (
       <div>
-        <h1>Add User</h1>
+        <h1>Have an Idea</h1>
         <form onSubmit={this.submit}>
-          Name:{" "}
+          Concept:{" "}
           <input
-            onChange={(e) => this.setState({ name: e.target.value })}
+            onChange={(e) => this.setState({ concept: e.target.value })}
             type="text"
             value={this.state.name}
           />
@@ -34,4 +35,14 @@ class ProjectForm extends Component {
   }
 }
 
-export default connect(null, { createProject })(ProjectForm);
+const mapStateToProps = ({ users }, props) => {
+  const userId = +props.match.params.id;
+  const user = users.all.find(function (u) {
+    return u.id === userId;
+  });
+  return {
+    user,
+  };
+};
+
+export default connect(mapStateToProps, { createProject })(ProjectForm);
