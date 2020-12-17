47b8aafa4de6ac4c048bacda05a4e1580924fa72
Ejo415 <ejo7220@yahoo.com>
2020-12-17 17:10:15 -0500

    got concept props displaying

--- app-sketchbook-front/src/components/UserShow.js
+++ app-sketchbook-front/src/components/UserShow.js
@@ -1,26 +1,47 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { getProjects } from "../redux/actions/projectActions";
+import ProjectListConcepts from "./ProjectListConcepts";
 
 class UserShow extends React.Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
     return (
       <div>
         <h1>User:</h1>
-        {this.props.user.name}
+        <h3>{this.props.user.name}</h3>
+        <br></br>{" "}
+        <Link to={`/project/${this.props.user.id}/new`}>
+          <button type="button">Have an Idea</button>
+        </Link>
+        <h4>Ideas</h4>
+        {this.props.project.map((project) => (
+          <ProjectListConcepts project={project} key={project.id} />
+        ))}
+        {console.log(this.props.projects)}
+        <h4>Projects</h4>
+        <h4>Completed Projects</h4>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ users }, props) => {
-  console.log(props);
+const mapStateToProps = ({ users, projects }, props) => {
   const userId = +props.match.params.id;
   const user = users.all.find(function (u) {
     return u.id === userId;
   });
+  const project = projects.all.filter(function (p) {
+    return p.user_id === userId;
+  });
   return {
     user,
+    project,
   };
 };
 
-export default connect(mapStateToProps)(UserShow);
+export default connect(mapStateToProps, { getProjects })(UserShow);
