ca9d2bc45313bab02d8a30f50f26a1b7a46cb785
Ejo415 <ejo7220@yahoo.com>
2020-12-18 19:36:39 -0500

    added edit user case in reducer

--- app-sketchbook-front/src/redux/reducers/projectReducer.js
+++ app-sketchbook-front/src/redux/reducers/projectReducer.js
@@ -2,13 +2,20 @@ function projectReducer(state = { all: [] }, action) {
   switch (action.type) {
     case "FETCH_PROJECTS_SUCCESS":
       return { ...state, all: action.payload };
-    //  default:
-    //   return state;
 
     case "CREATE_PROJECT_SUCCESS":
       return { ...state, all: [...state.all, action.payload] };
+    
+      
+      case 'EDIT_PROJECT_SUCCESS':
+    return {
+    ...state, all: [...state.all.filter(project => project.id !==action.payload.id), action.payload]
+         }
+
     default:
       return state;
+
+    
   }
 }
 
