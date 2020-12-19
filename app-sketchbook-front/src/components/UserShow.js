6da88e8befbd9dcd1a35af748408b56399e256de
Ejo415 <ejo7220@yahoo.com>
2020-12-18 19:35:43 -0500

    added imports for components

--- app-sketchbook-front/src/components/UserShow.js
+++ app-sketchbook-front/src/components/UserShow.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { getProjects } from "../redux/actions/projectActions";
 import ProjectListConcepts from "./ProjectListConcepts";
+import ProjectListTitles from "./ProjectListTitles";
+import ProjectListComplete from "./ProjectListComplete";
 
 class UserShow extends React.Component {
   componentDidMount() {
@@ -12,19 +14,25 @@ class UserShow extends React.Component {
   render() {
     return (
       <div>
-        <h1>User:</h1>
+        <h1>Welcome</h1>
         <h3>{this.props.user.name}</h3>
         <br></br>{" "}
         <Link to={`/project/${this.props.user.id}/new`}>
           <button type="button">Have an Idea</button>
         </Link>
         <h4>Ideas</h4>
-        {this.props.project.map((project) => (
+        {this.props.projects.map((project) => (
           <ProjectListConcepts project={project} key={project.id} />
         ))}
-        {console.log(this.props.projects)}
+        
         <h4>Projects</h4>
+        {this.props.projects.map((project) => (
+          <ProjectListTitles project={project} key={project.id} />
+        ))}
         <h4>Completed Projects</h4>
+        {this.props.projects.map((project) => (
+          <ProjectListComplete project={project} key={project.id} />
+        ))}
       </div>
     );
   }
@@ -40,7 +48,7 @@ const mapStateToProps = ({ users, projects }, props) => {
   });
   return {
     user,
-    project,
+    projects: project,
   };
 };
 
