0f36ed86850631a6b6982ed2d77b902419cd0b5b
Ejo415 <ejo7220@yahoo.com>
2020-12-18 19:34:43 -0500

    added left alignment

--- app-sketchbook-front/src/components/Nav.js
+++ app-sketchbook-front/src/components/Nav.js
@@ -4,14 +4,13 @@ import { NavLink } from "react-router-dom";
 const Nav = () => {
   return (
     <div>
-      <ul>
+      <ul style={{ textAlign: 'left'}}>
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
         <li>
           <NavLink to="/users">Users</NavLink>
         </li>
-
         <li>
           <NavLink to="/user/new">Create a user</NavLink>
         </li>
