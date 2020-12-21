ab559e3e95e9fa44ffcd7d5e7bd1340253e31038
Ejo415 <ejo7220@yahoo.com>
2020-12-21 17:09:24 -0500

    added a sort functionality

--- app-sketchbook-front/src/components/UsersIndex.js
+++ app-sketchbook-front/src/components/UsersIndex.js
@@ -3,20 +3,51 @@ import { connect } from "react-redux";
 import { getUsers } from "../redux/actions/userActions";
 import UserListItems from "./UserListItems";
 import Button from 'react-bootstrap/Button';
+//import ToggleButton from 'react-bootstrap/ToggleButton'
 
 class UsersIndex extends React.Component {
+
+    state = {
+        sortedList: false,
+      };
+
   componentDidMount() {
     this.props.getUsers();
   }
+
+    sortedUsers = () => { 
+        return ( [...this.props.users].sort(function(a, b) {
+    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+  
+    // names must be equal
+    return 0;
+  }));
+
+}
+
+  handleClick = () => {
+    this.setState(state => ({
+      sortedList: !state.sortedList
+    }));
+  }
+
+
   render() {
     return (
       <div>
       <Button size='sm' href="/user/new">New User</Button>
       <br></br>
       <br></br>
-      
+      <Button onClick={this.handleClick} >A-Z</Button>
         <h1>Users</h1>
-        {this.props.users.map((user) => (
+        {(this.state.sortedList ? this.sortedUsers() : this.props.users).map(user => (
           <UserListItems user={user} key={user.id} />
         ))}
       </div>
