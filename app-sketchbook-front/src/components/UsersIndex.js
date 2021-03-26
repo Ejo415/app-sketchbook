3549f49b9c106d5d9866adbc56820fe25c0838e1
Ejo415 <ejo7220@yahoo.com>
2021-03-26 08:59:12 -0400

    a thing

--- app-sketchbook-front/src/components/UsersIndex.js
+++ app-sketchbook-front/src/components/UsersIndex.js
@@ -17,16 +17,14 @@ class UsersIndex extends React.Component {
 
     sortedUsers = () => { 
         return ( [...this.props.users].sort(function(a, b) {
-    let nameA = a.name.toUpperCase(); // ignore upper and lowercase
-    let nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    let nameA = a.name.toUpperCase(); 
+    let nameB = b.name.toUpperCase(); 
     if (nameA < nameB) {
       return -1;
     }
     if (nameA > nameB) {
       return 1;
     }
-  
-    // names must be equal
     return 0;
   }));
 
